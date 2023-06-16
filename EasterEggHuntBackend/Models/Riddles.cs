c22e935a985cf5a5140f9f4d2bce2d4f8cdc469e
RuralYoung <ruralyoungiii@gmail.com>
2023-06-15 22:03:24 -0400

    Add project files.

---
+++ EasterEggHuntBackend/Models/Riddles.cs
@@ -0,0 +1,10 @@
+﻿namespace EasterEggHuntBackend.Models
+{
+    public class Riddles
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string Hint { get; set; }
+    }
+}
