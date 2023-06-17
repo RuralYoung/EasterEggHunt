3195ca7aacde0bf143d7fa21fefeb0bbf614704d
RuralYoung <ruralyoungiii@gmail.com>
2023-06-17 14:53:21 -0400

    Added a class that ensures that the data stored is populated

---
+++ EasterEggHuntBackend/Models/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EasterEggHuntBackend.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            RiddleDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<RiddleDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if (!context.Riddles.Any())
+            {
+                context.Riddles.AddRange(
+                    new Riddle
+                    {
+                        Question = "Who played the famous character Dr. Peter Venkman",
+                        Answer = "Billy Murray",
+                        Hint = "Murray Hill"
+                    }
+                );
+            }
+        }
+    }
+}
