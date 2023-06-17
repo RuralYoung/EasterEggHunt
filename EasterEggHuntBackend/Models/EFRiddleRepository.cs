4b527fe24be40b11866166f1c35193fbaaa20630
RuralYoung <ruralyoungiii@gmail.com>
2023-06-17 13:45:02 -0400

    Created the EFRiddleRepository in which maps the Riddles property in IRiddleRepository interface onto Riddles property in RiddleDbContext. The Riddles propery within RiddleDbContext returns DbSet<Riddle> object which implements IQuerable interface which allows for easier interfacibility (I based this off of the text in ASP.NET Core 6)

---
+++ EasterEggHuntBackend/Models/EFRiddleRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace EasterEggHuntBackend.Models
+{
+    public class EFRiddleRepository : IRiddleRepository
+    {
+        private RiddleDbContext context;
+
+        public EFRiddleRepository(RiddleDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Riddle> Riddles => context.Riddles;
+    }
+}
