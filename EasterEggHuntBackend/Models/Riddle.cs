ed40f4af87bb32623d6ca36829e2be2209ab66f8
RuralYoung <ruralyoungiii@gmail.com>
2023-06-19 16:25:39 -0400

    Added a ProgressCode to Riddle

--- EasterEggHuntBackend/Models/Riddle.cs
+++ EasterEggHuntBackend/Models/Riddle.cs
@@ -6,5 +6,6 @@
         public string Question { get; set; }
         public string Answer { get; set; }
         public string Hint { get; set; }
+        public int ProgressCode { get; set; }
     }
 }
