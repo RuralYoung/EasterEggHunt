1039f2fd68ff4151f8a01e7b1958623181603193
RuralYoung <ruralyoungiii@gmail.com>
2023-06-20 21:53:17 -0400

    Created a service that will get data for our riddles

---
+++ EasterEggHuntFrontend/src/services/get-riddles.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Riddle } from 'src/data/riddle';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetRiddlesService {
+
+  constructor() { }
+}
