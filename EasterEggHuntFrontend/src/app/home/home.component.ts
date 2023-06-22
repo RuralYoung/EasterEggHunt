200d378cc4e0f7df0dd8e9f39548b10c1c8a661b
RuralYoung <ruralyoungiii@gmail.com>
2023-06-22 15:58:37 -0400

    Home component now displays riddles

--- EasterEggHuntFrontend/src/app/home/home.component.ts
+++ EasterEggHuntFrontend/src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Riddle } from 'src/data/riddle';
+import { RiddlesService } from 'src/services/riddles.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  riddles: Riddle[] = [];
 
+  constructor( private riddleService: RiddlesService ) {}
+
+  ngOnInit(): void {
+    this.getRiddles();
+  }
+
+  getRiddles(): void {
+    this.riddleService.getRiddles()
+      .subscribe( riddles => this.riddles = riddles);
+  }
 }
