126eef2a30920b35bd1aef3bac0e19018b1108f0
RuralYoung <ruralyoungiii@gmail.com>
2023-06-26 19:00:38 -0400

    Added a button to display hints

--- EasterEggHuntFrontend/src/app/home/home.component.ts
+++ EasterEggHuntFrontend/src/app/home/home.component.ts
@@ -12,8 +12,10 @@ export class HomeComponent implements OnInit {
   riddles: Riddle[] = [];
   index: number = 0;
   currentRiddle: string = "";
+  currentHint: string = "";
   correctStatusMessage: string = "";
   correctStatusVisibility: boolean = false;
+  hintVisibility: boolean = false;
 
   constructor( private riddleService: RiddlesService ) {}
 
@@ -24,14 +26,15 @@ export class HomeComponent implements OnInit {
   getRiddles(): void {
     this.riddleService.getRiddles()
       .subscribe( serverRiddles => this.riddles = serverRiddles, 
-                  () => this.currentRiddle = "ERROR COULD NOT CONNECT TO QUESTION DATABASE", 
-                  () => this.currentRiddle = this.riddles[0].question);
+                  () => this.currentRiddle = "ERROR COULD NOT CONNECT TO QUESTION DATABASE", // Error case
+                  () => this.currentRiddle = this.riddles[0].question); // When subscribe is done
   }
 
   submitAnswer( userAnswer: string ): void {
     if ( this.riddles[this.index].answer.toLowerCase() == userAnswer.toLowerCase() ) {
       this.displayResponseMessage("Correct!");
-
+      this.hintVisibility = false;
+      
       // This is temporary, when you win, you should go to a win screen.
       ++this.index < this.riddles.length ? this.currentRiddle = this.riddles[this.index].question : this.currentRiddle = "You Win!";
     }
@@ -49,5 +52,10 @@ export class HomeComponent implements OnInit {
     setTimeout( 
       () => {this.correctStatusVisibility = false, this.correctStatusMessage = ""}, 2000 
     );
-}
+  }
+
+  displayHint(): void {
+    this.currentHint = this.riddles[this.index].hint;
+    this.hintVisibility = true;
+  }
 }
