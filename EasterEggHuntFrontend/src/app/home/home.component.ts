021162801907ff9e1c3b0edaa038dcb86382224d
RuralYoung <ruralyoungiii@gmail.com>
2023-06-25 23:59:10 -0400

    Added a message that shows if you are correct or incorrect.

--- EasterEggHuntFrontend/src/app/home/home.component.ts
+++ EasterEggHuntFrontend/src/app/home/home.component.ts
@@ -12,6 +12,8 @@ export class HomeComponent implements OnInit {
   riddles: Riddle[] = [];
   index: number = 0;
   currentRiddle: string = "";
+  correctStatusMessage: string = "";
+  correctStatusVisibility: boolean = false;
 
   constructor( private riddleService: RiddlesService ) {}
 
@@ -27,17 +29,25 @@ export class HomeComponent implements OnInit {
   }
 
   submitAnswer( userAnswer: string ): void {
-    console.log(userAnswer);
+    if ( this.riddles[this.index].answer.toLowerCase() == userAnswer.toLowerCase() ) {
+      this.displayResponseMessage("Correct!");
+
+      // This is temporary, when you win, you should go to a win screen.
+      ++this.index < this.riddles.length ? this.currentRiddle = this.riddles[this.index].question : this.currentRiddle = "You Win!";
+    }
+    else
+    {
+      if (this.correctStatusVisibility)
+        return;
+      this.displayResponseMessage("Incorrect!");
+    }
   }
 
-  /*nextButton(): void {
-    if ( this.index < this.riddles.length )
-    {
-      this.currentRiddle = this.riddles[this.index++]?.question;
-    }
-    else {
-      this.index = 0;
-      this.currentRiddle = this.riddles[this.index++]?.question;
-    }
-  }*/
+  displayResponseMessage(respMsg: string): void {
+    this.correctStatusMessage = respMsg;
+    this.correctStatusVisibility = true;
+    setTimeout( 
+      () => {this.correctStatusVisibility = false, this.correctStatusMessage = ""}, 2000 
+    );
+}
 }
