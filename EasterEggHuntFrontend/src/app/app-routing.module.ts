c3763110e6b1e77e28271c86520e97d0c53cce40
RuralYoung <ruralyoungiii@gmail.com>
2023-06-26 22:38:55 -0400

    Added routing

--- EasterEggHuntFrontend/src/app/app-routing.module.ts
+++ EasterEggHuntFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingPageComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
